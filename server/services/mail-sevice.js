2ee073d7b8afa1f6214fc06c6c6e0f820fa432ce
Denis Karpiuk <dzianiskarpiuk@gmail.com>
2022-08-15 18:35:49 +0300

    add sernd email function

--- server/services/mail-sevice.js
+++ server/services/mail-sevice.js
@@ -1,5 +1,31 @@
+const nodemailer = require('nodemailer')
+
 class MailService {
-    async sendActivationMail(to, link) {}
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'dzianiskarpiuk@gmail.com',
+                pass: 'baymgscsvrqcmyvf',
+            },
+        })
+    }
+
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: 'dzianiskarpiuk@gmail.com',
+            to,
+            subject: 'Activation account' + process.env.API_URL,
+            text: '',
+            html: `
+            <div>
+                <h1>Для активации перйдите по ссылке</h1>
+                <a href="${link}">${link}</a>
+            </div>`,
+        })
+    }
 }
 
 module.exports = new MailService()
